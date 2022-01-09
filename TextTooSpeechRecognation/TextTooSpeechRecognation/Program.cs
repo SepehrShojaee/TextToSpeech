bb8597481f2227eb56577c000365384aa012fc3a
Sepehr Shojaee <3pehr.shojaee@gmail.com>
2022-01-09 22:12:50 +0330

    Delete Zip File

---
+++ TextTooSpeechRecognation/TextTooSpeechRecognation/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TextTooSpeechRecognation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text;
+            Console.WriteLine("Enter the text to speech and press Enter:");
+            text = Console.ReadLine();
+            SpeechSynthesizer speechSynthesizer = new SpeechSynthesizer();
+            speechSynthesizer.SetOutputToDefaultAudioDevice();
+            
+            speechSynthesizer.Speak(text);
+            Console.WriteLine("Press any key...");
+            Console.ReadKey();
+        }
+    }
+}
